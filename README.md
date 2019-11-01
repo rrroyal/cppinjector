10edf6873e4329efdda9cef758a2be11008a26f0
royal <royal@shameful.xyz>
2019-11-02 00:01:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# cppinjector
+C++ Windows DLL injector with interactive mode.
+
+## Building
+**Use built binaries in [releases](https://github.com/rrroyal/cppinjector/releases), or make it yourself:**
+- Compile with VS2017/2019, `Windows 10 SDK 18362` and `Platform Toolkit v142`.
---
+++ cppinjector/cppinjector.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+DWORD getProcess(char* processName) {
+	HANDLE hPID = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	PROCESSENTRY32 procEntry;
+	procEntry.dwSize = sizeof(procEntry);
+
+	do {
+		if (!strcmp(procEntry.szExeFile, processName)) {
+			DWORD dwPID = procEntry.th32ProcessID;
+			CloseHandle(hPID);
+
+			return dwPID;
+		}
+	}
+	while (Process32Next(hPID, &procEntry));
+}
+
+int main() {
+	// declare variables
+	char process[MAX_PATH];
+	char dll[MAX_PATH];
+
+	DWORD dwProcess;
+	char myDLL[MAX_PATH];
+
+	// ask for input
+	printf("process: ");
+	scanf_s("%259s", process, MAX_PATH - 1);
+
+	printf("dll: ");
+	scanf_s("%259s", dll, MAX_PATH - 1);
+
+	// get path of DLL
+	GetFullPathNameA(dll, MAX_PATH, myDLL, NULL);
+
+	// get process
+	dwProcess = getProcess(process);
+
+	// open handle and allocate memory
+	HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, dwProcess);
+	LPVOID allocatedMem = VirtualAllocEx(hProcess, NULL, sizeof(myDLL), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+	// actually "inject" DLL into process memory
+	WriteProcessMemory(hProcess, allocatedMem, myDLL, sizeof(myDLL), NULL);
+
+	// launch DLL
+	CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, allocatedMem, 0, 0);
+
+	// close handle
+	CloseHandle(hProcess);
+
+	// done
+	return 1;
+}
